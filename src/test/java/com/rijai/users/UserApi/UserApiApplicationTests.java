f8556a6ffe45b2d7f643741199c8ae7856b1b3d0
leogabriel2000 <leogabriel2000@yahoo.com>
2021-10-08 11:51:02 +0800

    inital commit

---
+++ src/test/java/com/rijai/users/UserApi/UserApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rijai.users.UserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
